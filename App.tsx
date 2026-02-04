4e463b0d5a59428175393698f78d2a58966f19f2
Eduardo <eduardo.robettibedin@gmail.com>
2026-02-03 22:59:51 -0300

    feat(Notifee): enhance notification handling with background events and scheduling

--- App.tsx
+++ App.tsx
@@ -1,4 +1,10 @@
-import { Button, StatusBar, useColorScheme, View } from 'react-native';
+import {
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  useColorScheme,
+  View,
+} from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { useEffect, useState } from 'react';
 import Notifee, {
@@ -6,6 +12,8 @@ import Notifee, {
   AuthorizationStatus,
   Event,
   EventType,
+  TimestampTrigger,
+  TriggerType,
 } from '@notifee/react-native';
 
 function App() {
@@ -27,6 +35,24 @@ function App() {
     }
     getNotifications();
   }, []);
+
+  // Background: handle press and dismiss events when the app is closed
+  Notifee.onBackgroundEvent(async (event: Event) => {
+    if (event.type === EventType.DISMISSED) {
+      console.log('Notification dismissed from background');
+      return;
+    }
+    if (event.type === EventType.PRESS) {
+      if (event.detail?.notification?.id) {
+        await Notifee.cancelNotification(event.detail?.notification?.id);
+      }
+      console.log(
+        'Notification pressed from background',
+        event?.detail?.notification,
+      );
+      return;
+    }
+  });
 
   useEffect(() => {
     // Foreground: handle press and dismiss events while the app is open
@@ -64,11 +90,57 @@ function App() {
     });
   }
 
+  async function handleScheduleNotification() {
+    if (!notificationPermission) {
+      return;
+    }
+    const date = new Date(Date.now());
+    date.setMinutes(date.getMinutes() + 1);
+
+    const triggerNotification: TimestampTrigger = {
+      type: TriggerType.TIMESTAMP,
+      timestamp: date.getTime(),
+    };
+    await Notifee.createTriggerNotification(
+      {
+        title: 'Hello World in the future',
+        body: 'This is a test notification',
+        android: {
+          channelId: 'Test',
+          importance: AndroidImportance.HIGH,
+          pressAction: {
+            id: 'default',
+          },
+        },
+      },
+      triggerNotification,
+    );
+  }
+
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Button title="Send Notification" onPress={sendNotification} />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: 10,
+        }}
+      >
+        <TouchableOpacity
+          style={{ padding: 10, backgroundColor: 'blue', borderRadius: 5 }}
+          onPress={sendNotification}
+        >
+          <Text style={{ color: 'white' }}>Send Notification</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{ padding: 10, backgroundColor: 'green', borderRadius: 5 }}
+          onPress={handleScheduleNotification}
+        >
+          <Text style={{ color: 'white' }}>Schedule Notification</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaProvider>
   );
